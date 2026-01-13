494d5811f7bf79d3cd1eecc099cb230a597cf057
Rndraha21 <robinndraha111@gmail.com>
2026-01-13 20:12:50 +0700

    udpate deployment

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // Require necessary modules
 const RBot = require("./app/MyBot");
 const dotEnv = require("dotenv");
+const express = require("express");
 
 // Load environment variables from .env file
 dotEnv.config();
+
+const app = express();
 
 const token = process.env.TELEGRAM_TOKEN;
 const options = {
@@ -13,3 +16,10 @@ const options = {
 // Initialize and start the bot
 const rbot = new RBot(token, options);
 rbot.init();
+
+// Endpoint for healt check render
+app.get("/", (req, res) => res.send("Bot is running!"));
+
+app.listen(process.env.PORT || 3000, (req, res) => {
+  console.log("Bot is running");
+});
